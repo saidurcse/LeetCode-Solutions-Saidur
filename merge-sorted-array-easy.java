8a4c407344d5288bd46d1935d293dc81ba381eab
Saidur Rahman <saidur.rahman@bistasolutions.com>
2021-10-29 10:01:28 +0600

    merge sort added

--- merge-sorted-array-easy.java
+++ merge-sorted-array-easy.java
@@ -1,46 +1,13 @@
 import java.io.*;
 import java.util.*;
 
-public class Solution {
-
-	/*
-     * Given an array, find the contiguous subarray with the largest sum.
-     * Input: -4, 2,-5,1,2,3,6,-5,1
-     * Output: the largest sum subarray starts at index 3 and ends at 6 and the largest sum is 12
-     *
-     *
-     * Below solution has:
-     * Runtime Complexity - Linear, O(n).
-     * Memory Complexity - Constant, O(1).
-     *
-     * This can be solved by Kadane's algorithm:
-     * Basic idea of Kadane's algorithm is scanning the entire array and at each position finding the maximum sum of
-     * subarray ending there.
-     * This is achieved by keeping a current maximum for the current array index and a global maximum.
-     * */
-	
-    public static int maxSubArray(int[] nums) {
-    	
-    	int size = nums.length;
-        int maxSoFar = Integer.MIN_VALUE; 
-        int maxAtEnd = 0;
- 
-        for (int i = 0; i < size; i++)
-        {
-        	maxAtEnd = maxAtEnd + nums[i];
-            if (maxSoFar < maxAtEnd)
-            	maxSoFar = maxAtEnd;
-            if (maxAtEnd < 0)
-            	maxAtEnd = 0;
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        
+       for(int i = 0; i < n; i++){
+            nums1[m] = nums2[i];
+            m+=1;
         }
-        return maxSoFar;
-
-    }
-
-    public static void main(String[] args) {
-        int[] arr = {-4, 2,-5,1,2,3,6,-5,1};
-        System.out.println(maxSubArray(arr));
+        Arrays.sort(nums1);
     }
 }
-
-/* Output: 12 */
