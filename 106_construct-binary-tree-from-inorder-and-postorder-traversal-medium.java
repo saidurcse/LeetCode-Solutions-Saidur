91886bbcbd39f4fcbea62c181f3e8a2832beb0ec
Saidur Rahman <saidur.rahman@bistasolutions.com>
2021-12-20 16:15:50 +0600

    construct binary tree from postorder

---
+++ 106_construct-binary-tree-from-inorder-and-postorder-traversal-medium.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    
+    private int idx;
+    
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        if (inorder.length != postorder.length) return null;
+        if (inorder.length == 0) return null;
+        idx = postorder.length-1;
+        TreeNode root = build(inorder, postorder, 0, idx);
+        return root;
+    }
+    
+    private TreeNode build(int[] inorder, int[] postorder, int start, int end) {
+        if (start>end) return null;
+        TreeNode node = new TreeNode(postorder[idx--]);
+        if (start==end) return node;
+        
+        int index = findIdx(inorder, node.val, end);
+        node.right = build(inorder, postorder, index+1, end);
+        node.left = build(inorder, postorder, start, index-1);
+        return node;
+    }
+    
+    private int findIdx(int[] inorder, int val, int end) {
+        for (int i=end; i>=0; i--) {
+            if (inorder[i]==val) return i;
+        }
+        return 0;
+    }
+}
+// TC: O(N^2)
+// AS: O(N)
+// The worst case falls into the case when tree is a skew tree
