ae9cb01e431e17e71328e7736daa705df82bb26b
Saidur Rahman <saidur.rahman@bistasolutions.com>
2021-12-23 11:25:02 +0600

    course schedule

---
+++ 207_course-schedule-medium.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+
+//Easy BFS Topological sort, Java
+
+/*
+
+https://leetcode.com/problems/course-schedule/discuss/58524/Java-DFS-and-BFS-solution
+
+*/
+
+class Solution {
+       public static boolean canFinish(int numCourses, int[][] prerequisites) {
+        if (numCourses <= 0)
+            return false;
+        Queue<Integer> queue = new LinkedList<>();
+        int[] inDegree = new int[numCourses];
+        for (int i = 0; i < prerequisites.length; i++) {
+            inDegree[prerequisites[i][1]]++;
+        }
+        for (int i = 0; i < inDegree.length; i++) {
+            if (inDegree[i] == 0)
+                queue.offer(i);
+        }
+        while (!queue.isEmpty()) {
+            int x = queue.poll();
+            for (int i = 0; i < prerequisites.length; i++) {
+                if (x == prerequisites[i][0]) {
+                    inDegree[prerequisites[i][1]]--;
+                    if (inDegree[prerequisites[i][1]] == 0)
+                        queue.offer(prerequisites[i][1]);
+                }
+            }
+        }
+        for (int i = 0; i < inDegree.length; i++) {
+            if (inDegree[i] != 0)
+                return false;
+        }
+        return true;
+    }
+}
