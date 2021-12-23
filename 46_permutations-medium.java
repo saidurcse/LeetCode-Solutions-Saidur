cb098aff469ad6189eb962ec320eaa3e99e65c3f
Saidur Rahman <saidur.rahman@bistasolutions.com>
2021-12-23 18:15:44 +0600

    permutations

---
+++ 46_permutations-medium.java
@@ -0,0 +1,89 @@
+import java.io.*;
+import java.util.*;
+
+/*
+the basic idea is, to permute n numbers, we can add the nth number into the resulting List<List<Integer>> from the n-1 numbers, in every possible position.
+
+For example, if the input num[] is {1,2,3}: First, add 1 into the initial List<List<Integer>> (let's call it "answer").
+
+Then, 2 can be added in front or after 1. So we have to copy the List in answer (it's just {1}), add 2 in position 0 of {1}, then copy the original {1} again, and add 2 in position 1. Now we have an answer of {{2,1},{1,2}}. There are 2 lists in the current answer.
+
+Then we have to add 3. first copy {2,1} and {1,2}, add 3 in position 0; then copy {2,1} and {1,2}, and add 3 into position 1, then do the same thing for position 3. Finally we have 2*3=6 lists in answer, which is what we want.
+*/
+
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        if (nums.length ==0) return ans;
+        List<Integer> l0 = new ArrayList<Integer>();
+        l0.add(nums[0]);
+        ans.add(l0);
+        for (int i = 1; i< nums.length; ++i){
+            List<List<Integer>> new_ans = new ArrayList<List<Integer>>(); 
+            for (int j = 0; j<=i; ++j){            
+               for (List<Integer> l : ans){
+                   List<Integer> new_l = new ArrayList<Integer>(l);
+                   new_l.add(j,nums[i]);
+                   new_ans.add(new_l);
+               }
+            }
+            ans = new_ans;
+        }
+        return ans;
+    }
+}
+    public List<List<Integer>> threeSum(int[] nums) {
+        
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        
+        if (nums.length < 3)
+            return result;
+        
+        Arrays.sort(nums);
+        
+        for (int i = 0; i < nums.length-2; ++i) {
+            
+            if (i != 0 && nums[i] == nums[i-1])
+                continue;
+            
+            int number = nums[i];
+            int low = i + 1;
+            int high = nums.length - 1;
+            
+            while (low < high) {
+                
+                int total = number + nums[low] + nums[high];
+                
+                if (total == 0) {
+                    
+                    List<Integer> list = Arrays.asList(nums[low], number, nums[high]);
+                    
+                    result.add(list);
+                    
+                    low++;
+                    
+                    while (low < high && nums[low] == nums[low-1]) {
+                        low++;
+                    }
+                    
+                    high--;
+                    
+                    while (low < high && nums[high] == nums[high+1]) {
+                        high--;
+                    }
+                    
+                } else if (total < 0) {
+                    
+                    low++;
+                    
+                } else {
+                    
+                    high--;
+                }
+            }
+        }
+        
+        return result;
+    
+    }
+}
