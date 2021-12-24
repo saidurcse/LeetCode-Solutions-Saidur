30fdb0eee90ba5cca8eb853d752dfb31b10927bb
Saidur Rahman <saidur.rahman@bistasolutions.com>
2021-12-24 10:21:03 +0600

    permutations ii

---
+++ 47_permutations-ii-medium.java
@@ -0,0 +1,117 @@
+import java.io.*;
+import java.util.*;
+
+/*
+public class Solution { // Here's an iterative solution which doesn't use nextPermutation helper. It builds the permutations for i-1 first elements of an input array and tries to insert the ith element into all positions of each prebuilt i-1 permutation. I couldn't come up with more effective controling of uniqueness than just using a Set.
+    public List<List<Integer>> permuteUnique(int[] num) {
+        LinkedList<List<Integer>> res = new LinkedList<>();
+        res.add(new ArrayList<>());
+        for (int i = 0; i < num.length; i++) {
+            Set<String> cache = new HashSet<>();
+            while (res.peekFirst().size() == i) {
+                List<Integer> l = res.removeFirst();
+                for (int j = 0; j <= l.size(); j++) {
+                    List<Integer> newL = new ArrayList<>(l.subList(0,j));
+                    newL.add(num[i]);
+                    newL.addAll(l.subList(j,l.size()));
+                    if (cache.add(newL.toString())) res.add(newL);
+                }
+            }
+        }
+        return res;
+    }
+}
+*/
+
+class Solution {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (nums == null || nums.length == 0) {
+            return result;
+        }
+        perm(result, nums, 0, nums.length - 1);
+        return result;
+    }
+
+    public void perm(List<List<Integer>> result, int[] nums, int start, int end) {
+        Set<Integer> swapedNum = new HashSet<>();
+        if (start >= end) {
+            List<Integer> list = new ArrayList<>();
+            for (int num : nums) {
+                list.add(num);
+            }
+            result.add(list);
+        } else {
+            for (int i = start; i <= end; i++) {
+                if (swapedNum.add(nums[i])) {
+                    swap(nums, i, start);
+                    perm(result, nums, start + 1, end);
+                    swap(nums, i, start);
+                }
+            }
+        }
+    }
+
+
+    public void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+}
+}
+    public List<List<Integer>> threeSum(int[] nums) {
+        
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        
+        if (nums.length < 3)
+            return result;
+        
+        Arrays.sort(nums);
+        
+        for (int i = 0; i < nums.length-2; ++i) {
+            
+            if (i != 0 && nums[i] == nums[i-1])
+                continue;
+            
+            int number = nums[i];
+            int low = i + 1;
+            int high = nums.length - 1;
+            
+            while (low < high) {
+                
+                int total = number + nums[low] + nums[high];
+                
+                if (total == 0) {
+                    
+                    List<Integer> list = Arrays.asList(nums[low], number, nums[high]);
+                    
+                    result.add(list);
+                    
+                    low++;
+                    
+                    while (low < high && nums[low] == nums[low-1]) {
+                        low++;
+                    }
+                    
+                    high--;
+                    
+                    while (low < high && nums[high] == nums[high+1]) {
+                        high--;
+                    }
+                    
+                } else if (total < 0) {
+                    
+                    low++;
+                    
+                } else {
+                    
+                    high--;
+                }
+            }
+        }
+        
+        return result;
+    
+    }
+}
