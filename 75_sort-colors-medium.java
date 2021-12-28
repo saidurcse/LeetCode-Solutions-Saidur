be975a56508844baf6f74074f7de07014300965d
Saidur Rahman <saidurcse@users.noreply.github.com>
2021-12-28 18:02:53 +0600

    Update 75_sort-colors-medium.java

--- 75_sort-colors-medium.java
+++ 75_sort-colors-medium.java
@@ -7,9 +7,11 @@ If you are not familiar with quick sort partition I suggest you get to know that
 Main Idea:
 
 this solution will partition the array into 3 parts, 0,1,2 instead of just 2 parts like quick sort. We just put 0 on the left side, 2 on the right side.
-we have two pointers low and high which indicate the bound of 0s and 2s (low is the right bound of 0 and high is the left bound of 2). At the end of this partiton low and high will be like this.
+we have two pointers low and high which indicate the bound of 0s and 2s (low is the right bound of 0 and high is the left bound of 2).
+At the end of this partiton low and high will be like this.
 
-During the partition, one thing is for sure: left side of low pointer will be all 0s and right side of high pointer will be all 2s. This is guranteed by swaping nums[mid] with the number[low + 1] when nums[mid] == 0 and with number[high - 1] when nums[mid] == 2。
+During the partition, one thing is for sure: left side of low pointer will be all 0s and right side of high pointer will be all 2s.
+This is guranteed by swaping nums[mid] with the number[low + 1] when nums[mid] == 0 and with number[high - 1] when nums[mid] == 2。
 */
 
 class Solution {
