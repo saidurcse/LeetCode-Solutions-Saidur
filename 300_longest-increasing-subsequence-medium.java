515c0bfa9f5c77e3d8f702cffa53bd5ce02da5e5
Saidur Rahman <saidurcse@users.noreply.github.com>
2021-12-28 13:49:26 +0600

    Update 300_longest-increasing-subsequence-medium.java

--- 300_longest-increasing-subsequence-medium.java
+++ 300_longest-increasing-subsequence-medium.java
@@ -22,7 +22,6 @@ confusing. The problem can be included as a DP problem but it doesn't make sense
 I came up with was stack...
 */
 
-/*
 public int lengthOfLIS(int[] nums) {
   if (nums.length == 0) return 0;
   
@@ -30,41 +29,16 @@ public int lengthOfLIS(int[] nums) {
   int[] dp = new int[n];
   
   for (int i = 0; i < n; i++) {
-    dp[i] = 1;
+    dp[i] = 1; /* Initialize LIS values for all indexes */
     
-    for (int j = 0; j < i; j++) {
+    for (int j = 0; j < i; j++) { /* Compute optimized LIS values in bottom up manner */
       if (nums[i] > nums[j] && dp[j] + 1 > dp[i]) {
         dp[i] = dp[j] + 1;
       }
     }
     
-    max = Math.max(max, dp[i]);
+    max = Math.max(max, dp[i]); /* Pick maximum of all LIS values */
   }
   
   return max;
-}
-*/
-
-class Solution {
-    public int lengthOfLIS(int[] nums) {
-        int lis[] = new int[nums.length];
-        int i, j, max = 0;
-  
-        /* Initialize LIS values for all indexes */
-        for (i = 0; i < nums.length; i++)
-            lis[i] = 1;
-  
-        /* Compute optimized LIS values in bottom up manner */
-        for (i = 1; i < nums.length; i++)
-            for (j = 0; j < i; j++)
-                if (nums[i] > nums[j] && lis[i] < lis[j] + 1)
-                    lis[i] = lis[j] + 1;
-  
-        /* Pick maximum of all LIS values */
-        for (i = 0; i < nums.length; i++)
-            if (max < lis[i])
-                max = lis[i];
-  
-        return max;
-    }
 }
