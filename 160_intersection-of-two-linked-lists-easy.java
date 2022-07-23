86be014664e8ac3f1052ec6d6221922131b360e6
Saidur Rahman <saidurcse@users.noreply.github.com>
2022-07-23 18:19:38 +0600

    Update 160_intersection-of-two-linked-lists-easy.java

--- 160_intersection-of-two-linked-lists-easy.java
+++ 160_intersection-of-two-linked-lists-easy.java
@@ -12,13 +12,20 @@ import java.util.*;
  *     }
  * }
  */
+/*
+I found most solutions here preprocess linkedlists to get the difference in len.
+Actually we don't care about the "value" of difference, we just want to make sure two pointers reach the intersection node at the same time.
+*/
+
 public class Solution {
     
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         ListNode p1, p2;
         p1 = headA;
         p2 = headB;
+        //if p1 & p2 have different len, then we will stop the loop after second iteration
         while (p1 != p2) {
+            //for the end of first iteration, we just reset the pointer to the head of another linkedlist
             if (p1 == null) {
                 p1 = headB;
             } else {
@@ -30,6 +37,7 @@ public class Solution {
                 p2 = p2.next;
             }
         }
+        //boundary check
         if (p1 == null) {
             return null;
         }
