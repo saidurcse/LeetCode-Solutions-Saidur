09364d9f1ec2b53fcc5498e40504d7a2f925fd1c
Saidur Rahman <saidur.rahman@bistasolutions.com>
2023-04-02 03:11:12 -0500

    New Solutons Added

---
+++ 338_counting-bits-easy.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public int[] countBits(int n) {
+        int[] f = new int[n+1];
+
+        for(int i=1;i<=n; i++) {
+            f[i] = f[i>>1] + (i&1);
+        }
+
+        return f;
+    }
+}
