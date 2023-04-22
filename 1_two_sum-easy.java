f71cb548a30da6a0f313ab3118dcc185f5195552
Saidur Rahman <saidurcse@users.noreply.github.com>
2023-04-22 16:44:07 -0500

    Update 1_two_sum-easy.java

--- 1_two_sum-easy.java
+++ 1_two_sum-easy.java
@@ -38,3 +38,22 @@ class Solution {
         return result;
     }
 }
+
+/*
+class Solution {
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+        val result = IntArray(2)
+
+        for(i in 0 until nums.size - 1) {
+            for(j in i+1 until nums.size) {
+                if(nums.get(i) + nums.get(j) == target) {
+                    result[0] = i
+                    result[1] = j
+                }
+            }
+        }
+
+        return result
+    }
+}
+*/
