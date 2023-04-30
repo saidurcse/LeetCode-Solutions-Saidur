ec060ac84bfdfb54b41c52a7e7c0300ed94423c3
Saidur Rahman <saidurcse@users.noreply.github.com>
2023-04-30 15:13:39 -0500

    Update 191_number-of-1-Bits-easy.java

--- 191_number-of-1-Bits-easy.java
+++ 191_number-of-1-Bits-easy.java
@@ -1,10 +1,18 @@
 import java.io.*;
 import java.util.*;
 
+/*
 public class Solution {
     // you need to treat n as an unsigned value
     public int hammingWeight(int n) {
         
         return Integer.bitCount(n);
+    }
+}*/
+
+class Solution {
+    // you need treat n as an unsigned value
+    fun hammingWeight(n:Int):Int {
+        return Integer.bitCount(n)
     }
 }
