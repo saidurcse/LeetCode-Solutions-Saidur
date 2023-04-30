1eca42099daa1f1265e8cfe66aeb64ba087b9964
Saidur Rahman <saidurcse@users.noreply.github.com>
2023-04-30 15:28:43 -0500

    Update 338_counting-bits-easy.java

--- 338_counting-bits-easy.java
+++ 338_counting-bits-easy.java
@@ -4,6 +4,7 @@ import java.util.*;
 // Using one array loop
 // Using bitwise operator counting the bits
 
+/*
 class Solution {
     public int[] countBits(int n) {
         int[] f = new int[n+1];
@@ -13,5 +14,18 @@ class Solution {
         }
 
         return f;
+    }
+}*/
+
+
+class Solution {
+    fun countBits(n: Int): IntArray {
+        val f = IntArray(n + 1)
+        
+        for (i in 1..n) {
+            f[i] = f[i shr 1] + (i and 1)
+        }
+
+        return f
     }
 }
