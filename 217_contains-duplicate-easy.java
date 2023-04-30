e332c221b34131cd9020c8febe7ff9c3a6d43f61
Saidur Rahman <saidurcse@users.noreply.github.com>
2023-04-30 14:41:34 -0500

    Update 217_contains-duplicate-easy.java

--- 217_contains-duplicate-easy.java
+++ 217_contains-duplicate-easy.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.util.*;
 
+/*
 class Solution {
     public boolean containsDuplicate(int[] nums) {
         Arrays.sort(nums); 
@@ -12,6 +13,7 @@ class Solution {
         return false;
     }
 }
+*/
 
 
 // Time complexity: O(n)
@@ -31,5 +33,19 @@ class Solution {
         }
         // Otherwise return false...
         return false;
+    }
+}
+
+/*Kotlin*/
+class Solution {
+    fun containsDuplicate(nums: IntArray): Boolean {
+        
+        Arrays.sort(nums)
+        for(i in 0 until nums.size-1) {
+            if(nums[i] == nums[i+1])
+                return true
+        }
+
+        return false
     }
 }
