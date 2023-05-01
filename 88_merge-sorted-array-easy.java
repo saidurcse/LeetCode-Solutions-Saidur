7e1662e7939fb5e4ec728471f78affc3e63191a8
Saidur Rahman <saidurcse@users.noreply.github.com>
2023-05-01 10:22:23 -0500

    Update 88_merge-sorted-array-easy.java

--- 88_merge-sorted-array-easy.java
+++ 88_merge-sorted-array-easy.java
@@ -15,11 +15,9 @@ class Solution {
 /*Kotlin*/
 class Solution {
     fun merge(nums1: IntArray, m: Int, nums2: IntArray, n: Int): Unit {
-        
         for(i in 0 until nums2.size) {
             nums1[m+i] = nums2[i]
         }
         nums1.sort()
-
     }
 }
